36fc97f28a8127cdec5a34b338114de6d28aa7fe
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2024-06-20 19:41:58 +0530

    Update test_example.py

--- api/testcases/test_example.py
+++ api/testcases/test_example.py
@@ -4,7 +4,7 @@ import requests
 class TestAPI(unittest.TestCase):
     def test_api_response(self):
         response = requests.get("http://localhost:3000/api/endpoint")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
 
 if __name__ == '__main__':
     unittest.main()
