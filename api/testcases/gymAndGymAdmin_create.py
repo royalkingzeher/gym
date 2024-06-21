9e77f29357a263a127dbeff19cfb9ed3401c314c
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2024-06-21 09:17:26 +0530

    Fixes

--- api/testcases/gymAndGymAdmin_create.py
+++ api/testcases/gymAndGymAdmin_create.py
@@ -1,28 +1,26 @@
-import unittest
 import requests
+import unittest
 
 class TestCreateGymAndGymAdmin(unittest.TestCase):
+
+    def setUp(self):
+        self.base_url = 'http://localhost:3000'
+        self.login_url = f'{self.base_url}/api/auth/login/'
+        self.create_gymadmin_url = f'{self.base_url}/api/gym/create/'
+        self.credentials = {'username': 'testadmin', 'password': 'adminpassword'}
+
+        # Log in to get the token
+        login_response = requests.post(self.login_url, data=self.credentials)
+        if login_response.status_code == 200:
+            self.token = login_response.json().get('token')
+        else:
+            self.token = None
+
     def test_create_gym_admin_and_gym(self):
-        # Existing user credentials
-        username = 'gymsoftware'
-        password ='gymsoftware'
-        # Send POST request
-        response = requests.post('http://localhost:3000/api/login', json={
-            'username': username,
-            'password': password
-        })
-        # Assert status code
-        self.assertEqual(response.status_code, 400)
-        # Assert response body contains token
-        self.token = response.json().get('token')
-        self.headers = {'Authorization': f'Bearer {self.token}'}
-        # Send POST request
-        response = requests.post('http://localhost:3000/api/gymAndGymAdmin', json={
-            'gymAdminId': 12,
-            'gymId': 25
-        },headers=self.headers)
-        # Assert status code
-        self.assertEqual(response.status_code, 200)
-        
+        headers = {'Authorization': f'Token {self.token}'}
+        data = {'username': 'gymadmin', 'password': 'gympassword'}
+        response = requests.post(self.create_gymadmin_url, headers=headers, data=data)
+        self.assertEqual(response.status_code, 201, response.text)
+
 if __name__ == '__main__':
     unittest.main()
