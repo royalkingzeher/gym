3c5faf103f8a4af881e190d09bda97809bf2efa0
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2024-06-21 09:44:29 +0530

    Update gymAndGymAdmin_getByGymAdminID.py

--- api/testcases/gymAndGymAdmin_getByGymAdminID.py
+++ api/testcases/gymAndGymAdmin_getByGymAdminID.py
@@ -4,10 +4,10 @@ import unittest
 class TestGetGymsOfGymAdmin(unittest.TestCase):
 
     def setUp(self):
-        self.base_url = 'http://localhost:3000'
+        self.base_url = 'http://localhost:8000'
         self.login_url = f'{self.base_url}/api/auth/login/'
-        self.get_by_gymadmin_url = f'{self.base_url}/api/gym/gymadmin/{gymadmin_id}/'
         self.credentials = {'username': 'testadmin', 'password': 'adminpassword'}
+        self.gymadmin_id = 22  # Replace with a valid gymadmin_id
 
         # Log in to get the token
         login_response = requests.post(self.login_url, data=self.credentials)
@@ -15,6 +15,8 @@ class TestGetGymsOfGymAdmin(unittest.TestCase):
             self.token = login_response.json().get('token')
         else:
             self.token = None
+
+        self.get_by_gymadmin_url = f'{self.base_url}/api/gym/gymadmin/{self.gymadmin_id}/'
 
     def test_get_gyms_of_gym_admin(self):
         headers = {'Authorization': f'Token {self.token}'}
