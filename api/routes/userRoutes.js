c80594367dc072d3032b7d3b1a63c74b07ffc3fe
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2024-06-23 16:51:23 +0530

    Update userRoutes.js

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const userController = require("../controllers/userController");
 
-router.get("/:userId", authMiddleware, userController.getUserById);
+router.get("/:id", authMiddleware, userController.getUserById);
 router.get("/", authMiddleware, userController.getAllUsers);
-router.put("/:userId", authMiddleware, userController.updateUser);
-router.delete("/:userId", authMiddleware, userController.deleteUser);
+router.put("/:id", authMiddleware, userController.updateUser);
+router.delete("/:id", authMiddleware, userController.deleteUser);
 
 module.exports = router;
