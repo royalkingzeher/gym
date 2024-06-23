96c041bd28ef3bf27f23e98b32fd9c1c050e9387
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2024-06-23 16:49:08 +0530

    Sync

--- api/routes/gymMembershipPlanRoutes.js
+++ api/routes/gymMembershipPlanRoutes.js
@@ -6,11 +6,15 @@ const {
   getMembershipPlanById,
   getMembershipPlansByGymId,
   updateMembershipPlanById,
+  deleteMembershipPlanById,
+  getAllMembershipPlans,
 } = require("../controllers/gymMembershipPlanController");
 
 router.post("/", authMiddleware, createGymMembershipPlan);
+router.get("/", authMiddleware, getAllMembershipPlans);
 router.get("/:planId", authMiddleware, getMembershipPlanById);
 router.get("/allByGym/:gymId", authMiddleware, getMembershipPlansByGymId);
 router.put("/update/:planId", authMiddleware, updateMembershipPlanById);
+router.delete("/delete/:planId", authMiddleware, deleteMembershipPlanById);
 
 module.exports = router;
