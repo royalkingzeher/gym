727a03e33a17ab7b441aa51b33ca5c7cf270845a
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2024-06-23 13:48:37 +0530

    Fix

--- api/testcases/test_payment_controller.py
+++ api/testcases/test_payment_controller.py
@@ -22,12 +22,14 @@ class TestPaymentController(unittest.TestCase):
             "comments": "First payment"
         }
         response = requests.post(self.BASE_URL, json=payload, headers=self.HEADERS)
+        if response.status_code != 201:
+            print(f"Response: {response.json()}")
         self.assertEqual(response.status_code, 201)
         self.assertIn('id', response.json())
 
     def test_create_payment_bad_request(self):
         payload = {
-            "gym_member_id": "invalid_id",  # Invalid type to trigger a bad request
+            "gym_member_id": "5",  # Invalid type to trigger a bad request
             "membership_plan_id": 2,
             "start_date": "2024-06-01",
             "end_date": "2024-12-01",
@@ -75,6 +77,8 @@ class TestPaymentController(unittest.TestCase):
             "comments": "First payment"
         }
         response = requests.post(f"{self.BASE_URL}/simulate_internal_error", json=payload, headers=self.HEADERS)
+        if response.status_code != 500:
+            print(f"Response: {response.json()}")
         self.assertEqual(response.status_code, 500)
 
 if __name__ == '__main__':
