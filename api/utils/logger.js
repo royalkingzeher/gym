314eb36c1e0263dee4e101362afe6c1294e2c1b2
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2024-06-23 12:59:11 +0530

    Create logger.js

---
+++ api/utils/logger.js
@@ -0,0 +1,20 @@
+// utils/logger.js
+
+const { createLogger, transports, format } = require("winston");
+
+const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "error.log", level: "error" }),
+    new transports.File({ filename: "combined.log" }),
+  ],
+});
+
+module.exports = logger;
