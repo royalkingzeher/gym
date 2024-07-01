8305b9f1ddb208c52683a79b2bf6fc6c4232bd0b
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2024-07-01 12:06:02 +0530

    Update authController.js

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -3,8 +3,12 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const GymAndGymMember = require("../models/gymAndGymMember");
+const GymAndGymAdmin = require("../models/gymAndGymAdmin");
 const Gym = require("../models/gym");
 const logger = require("../utils/logger");
+const MembershipPlan = require("../models/gymMembershipPlan");
+const MembersMembership = require("../models/membersMembership");
+const MembershipPlansPrice = require("../models/membershipPlansPrice");
 
 dotenv.config(); // Load environment variables
 
@@ -27,18 +31,54 @@ dotenv.config(); // Load environment variables
  *         application/json:
  *           schema:
  *             type: object
+ *             required:
+ *               - username
+ *               - password
  *             properties:
  *               username:
  *                 type: string
+ *                 description: The username for the new admin user
  *               password:
  *                 type: string
+ *                 description: The password for the new admin user
  *                 minLength: 8  # Password must be at least 8 characters long
  *     responses:
  *       200:
  *         description: The admin user was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     username:
+ *                       type: string
  *       400:
  *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
  */
+
 exports.signupAdmin = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -96,6 +136,10 @@ exports.signupAdmin = async (req, res) => {
  *         application/json:
  *           schema:
  *             type: object
+ *             required:
+ *               - username
+ *               - password
+ *               - firstName
  *             properties:
  *               username:
  *                 type: string
@@ -141,11 +185,53 @@ exports.signupAdmin = async (req, res) => {
  *     responses:
  *       200:
  *         description: The gym admin user was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Gym admin user created successfully
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     username:
+ *                       type: string
  *       400:
  *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Username, password, and first name are required.
  *       403:
  *         description: Forbidden, only admin user can create gym admin user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Forbidden, only admin user can create gym admin user
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Internal server error
  */
+
 exports.signupGymAdmin = async (req, res) => {
   try {
     const {
@@ -257,6 +343,11 @@ exports.signupGymAdmin = async (req, res) => {
  *         application/json:
  *           schema:
  *             type: object
+ *             required:
+ *               - username
+ *               - password
+ *               - firstName
+ *               - gymId
  *             properties:
  *               username:
  *                 type: string
@@ -304,10 +395,41 @@ exports.signupGymAdmin = async (req, res) => {
  *     responses:
  *       200:
  *         description: The gym member user was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Gym member user created successfully
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     username:
+ *                       type: string
  *       400:
  *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Username, password, first name, and gym ID are required.
  *       500:
  *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Internal server error
  */
 exports.signupGymMember = async (req, res) => {
   try {
@@ -420,17 +542,48 @@ exports.signupGymMember = async (req, res) => {
  *         application/json:
  *           schema:
  *             type: object
+ *             required:
+ *               - username
+ *               - password
  *             properties:
  *               username:
  *                 type: string
+ *                 example: johndoe
  *               password:
  *                 type: string
  *                 minLength: 8  # Password must be at least 8 characters long
+ *                 example: password123
  *     responses:
  *       200:
  *         description: The user was successfully logged in
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjA2NjY2MjU4LCJleHAiOjE2MDY2Njk4NTh9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
  *       400:
  *         description: Invalid username or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Invalid username or password.
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Internal server error
  */
 exports.login = async (req, res) => {
   try {
@@ -460,15 +613,85 @@ exports.login = async (req, res) => {
  * @swagger
  * /api/userDetails:
  *   post:
- *     summary: Get user details
+ *     summary: Get current user details
  *     tags: [Auth]
  *     security:
  *       - bearerAuth: []
  *     responses:
  *       200:
  *         description: The user details are successfully retrieved
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 1
+ *                 username:
+ *                   type: string
+ *                   example: johndoe
+ *                 firstName:
+ *                   type: string
+ *                   example: John
+ *                 lastName:
+ *                   type: string
+ *                   example: Doe
+ *                 email:
+ *                   type: string
+ *                   format: email
+ *                   example: johndoe@example.com
+ *                 phone:
+ *                   type: string
+ *                   example: 1234567890
+ *                 address:
+ *                   type: string
+ *                   example: 123 Main St
+ *                 city:
+ *                   type: string
+ *                   example: Anytown
+ *                 state:
+ *                   type: string
+ *                   example: Anystate
+ *                 pincode:
+ *                   type: string
+ *                   example: 123456
+ *                 country:
+ *                   type: string
+ *                   example: Anycountry
+ *                 dateOfBirth:
+ *                   type: string
+ *                   format: date
+ *                   example: 1990-01-01
+ *                 gender:
+ *                   type: string
+ *                   example: male
+ *                 profilePicture:
+ *                   type: string
+ *                   example: profile_picture_url
+ *                 emergencyContactName:
+ *                   type: string
+ *                   example: Jane Doe
+ *                 emergencyContactRelationship:
+ *                   type: string
+ *                   example: Spouse
+ *                 emergencyContactPhone:
+ *                   type: string
+ *                   example: 0987654321
+ *                 emergencyContactEmail:
+ *                   type: string
+ *                   format: email
+ *                   example: janedoe@example.com
  *       500:
  *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Internal server error
  */
 exports.userDetails = async (req, res) => {
   try {
@@ -481,6 +704,55 @@ exports.userDetails = async (req, res) => {
     res.json(user);
   } catch (error) {
     logger.error(`Error in userDetails: ${error.message}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+/**
+ * @swagger
+ * /api/deleteAll:
+ *   delete:
+ *     summary: Delete all data from the database
+ *     tags: [Auth]
+ *     responses:
+ *       200:
+ *         description: All data deleted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: All data deleted successfully
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ */
+exports.deleteAll = async (req, res) => {
+  try {
+    const currentUser = req.user;
+    if (currentUser.type !== "admin") {
+      return res.status(403).json({ error: "Forbidden, only admin user can delete all data" });
+    }
+    await MembershipPlansPrice.destroy({ where: {} });
+    await MembersMembership.destroy({ where: {} });
+    await MembershipPlan.destroy({ where: {} });
+    await GymAndGymMember.destroy({ where: {} });
+    await GymAndGymAdmin.destroy({ where: {} });
+    await Gym.destroy({ where: {} });
+    await User.destroy({ where: {} });
+
+
+    res.json({ message: "All data deleted successfully" });
+  } catch (error) {
+    logger.error(`Error in deleteAll: ${error.message}`);
     res.status(500).json({ error: "Internal server error" });
   }
 };
