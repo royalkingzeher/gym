eb67e6a43859ba375702670fa98bc27dcdfeebff
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2024-07-01 12:44:18 +0530

    Update test.py

--- api/testcases/test.py
+++ api/testcases/test.py
@@ -6,14 +6,18 @@ filename = "../setup_data.out"
 config = configparser.ConfigParser()
 config.read(filename)
 
-# Accessing values from the section
-ADMIN_TOKEN = config.get("SetupData", "ADMIN_TOKEN")
-GYM_ID = config.get("SetupData", "GYM_ID")
-GYM_MEMBER_ID = config.get("SetupData", "GYM_MEMBER_ID")
-GYM_ADMIN_ID = config.get("SetupData", "GYM_ADMIN_ID")
+# Check if the section exists
+if "SetupData" in config:
+    # Accessing values from the section
+    ADMIN_TOKEN = config.get("SetupData", "ADMIN_TOKEN", fallback=None)
+    GYM_ID = config.get("SetupData", "GYM_ID", fallback=None)
+    GYM_MEMBER_ID = config.get("SetupData", "GYM_MEMBER_ID", fallback=None)
+    GYM_ADMIN_ID = config.get("SetupData", "GYM_ADMIN_ID", fallback=None)
 
-# Example usage
-print(f"ADMIN_TOKEN: {ADMIN_TOKEN}")
-print(f"GYM_ID: {GYM_ID}")
-print(f"GYM_MEMBER_ID: {GYM_MEMBER_ID}")
-print(f"GYM_ADMIN_ID: {GYM_ADMIN_ID}")
+    # Example usage
+    print(f"ADMIN_TOKEN: {ADMIN_TOKEN}")
+    print(f"GYM_ID: {GYM_ID}")
+    print(f"GYM_MEMBER_ID: {GYM_MEMBER_ID}")
+    print(f"GYM_ADMIN_ID: {GYM_ADMIN_ID}")
+else:
+    print("SetupData section not found in the configuration file.")
